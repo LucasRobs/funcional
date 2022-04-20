569ac1aa546c9461657a9794613bb4d866e355e0
Robs <lucasrobs@alu.ufc.br>
2022-04-20 11:29:10 -0300

    Aula treino

--- testes/fold.hs
+++ testes/fold.hs
@@ -1,10 +1,18 @@
 import Control.Monad (when)
 
+contar x xs = foldl equal 0 xs
+  where
+    equal acc index = if x == index then acc + 1 else acc
+
+somarTodos xs = foldl somar 0 xs
+  where
+    somar acc index = acc + index
+
 menor xs = foldl qualMenor (head xs) xs
   where
-    qualMenor x y = if x > y then y else x
+    qualMenor menor' index = if menor' < index then menor' else index
 
-taNaLista x xs = foldl tem False xs
+existe x xs = foldl tem False xs
   where
     tem a y = a || x == y
 
@@ -12,6 +20,10 @@ pegaImpares xs = foldl pega [] xs
   where
     pega a y = if odd y then a ++ [y] else a
 
-contarRepetidos xs = foldl conta (acc, lest) xs
+contarRepetidos xs = fst $ foldl conta (0, -1) xs
   where
     conta (acc, lest) y = if lest == y then (acc + 1, y) else (acc, y)
+
+ehPrimo x = foldl isPrimo True [2 .. x - 1]
+  where
+    isPrimo acc index = acc && x `mod` index /= 0
